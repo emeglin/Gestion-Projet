55b3676832c8e72187c8e2df18bb90aca138c238
sebastien <sebastien.jurdeczka@sfr.fr>
2019-01-20 15:22:45 +0100

    added translated comments

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,8 @@
 // creation d'un tableau vierge afin de stocker les données json
+// create an empty array to store json data
 var listpromo = [];
 //declaration des variable pour la creation de promo
+//Create all variables for promotions
 var newPromo = document.querySelector('#newPromo');
 var createPromo = document.querySelector('#createPromo');
 var modifyPromo = document.querySelector('#modifyPromo');
@@ -11,12 +13,19 @@ var myDiv = document.querySelector("#myDiv");
 function getPromotion() {
     // Récupérez la liste des promotions via un fetch et affichez la via un console.log
 
+    // Get the promotion list thanks to fetch and display it with console.log
+
     fetch("http://api-students.popschool-lens.fr/api/promotions")
         // la reponse est envoyé par l'api puis recupérée en format json
+        // the response is sent by the api then converted in json
         .then(response => response.json())
         // le json devient promo est renvoyé par la console via console log
+
+        // json become promo and displayed by console
         .then(promo => {
             // la notation entre crochets m'a permi de selectionner uniquement la partie qui m'interesse
+
+        
             console.log(promo);
             console.log(promo['hydra:member']);
             listpromo = promo['hydra:member'];
@@ -24,7 +33,8 @@ function getPromotion() {
             mySelect.innerHTML = '';
             listpromo.forEach(promotion => {
                 myDiv.innerHTML += promotion.id + ". " + promotion.name + "<br>";
-                // ici je veux rajouter la liste dans le menu deroulant         
+                // ici je veux rajouter la liste dans le menu deroulant 
+                // here i add the list in my select menu        
                 var myOption = document.createElement('option');
                 myOption.innerHTML = promotion.name;
                 myOption.value = promotion.id
@@ -38,7 +48,8 @@ getPromotion();
 // creation d'un event listener pour le bouton
 createPromo.addEventListener('click', createPromotion);
 
-// fonction qui permettra de creer la fonction, la methode POST permettra de rajouter la nouvelle promotion a la liste deja pré-existante.
+// La methode POST permettra de rajouter la nouvelle promotion a la liste deja pré-existante.
+// POST method will allow to add a new promotion in the already existing list
 function createPromotion() {
     fetch("http://api-students.popschool-lens.fr/api/promotions", {
             headers: {
@@ -47,6 +58,7 @@ function createPromotion() {
             },
             method: "POST",
             // on transforme la valeur du champs input newPromo en chaine Json via stringify
+            // we transform the newPromo input value in Json
             body: JSON.stringify({
                 name: newPromo.value
             })
@@ -61,15 +73,19 @@ function createPromotion() {
 deletePromo.addEventListener('click', function () {
 
     // Je demande confirmation à l'utilisateur avant suppression 
+    // Asking the user to confirm the deletion
     if (confirm("Supprimer la promo : " + mySelect.value + " ?")) {
         // confirme la suppression
+        // deletion confirmed
         deletePromotion(mySelect.value);
         // ici le value correspond a celui que j'ai monté dans le DOM en l'allouant a l'option
+        // here the value is the one that i've created in the html page by allocating it in the option
     }
 })
 
 
 // C'est la fonction qui est déclarée pour s'occuper de la suppression
+// That's the declared function to do the deletion
 function deletePromotion(idPromo) {
     fetch("http://api-students.popschool-lens.fr/api/promotions/" + idPromo, {
             method: "DELETE"
@@ -82,11 +98,10 @@ function deletePromotion(idPromo) {
 
 modifyPromo.addEventListener('click', function () {
 
-    // Je demande confirmation à l'utilisateur avant suppression 
-    if (confirm("Modifier la promo : " + mySelect.value + " ?")) {
-        // confirme la suppression
-        modifyPromotion(mySelect.value);
-        // ici le value correspond a celui que j'ai monté dans le DOM en l'allouant a l'option
+    // Je demande confirmation à l'utilisateur avant modification
+    // Asking to the user for change confirming
+    if (confirm("Modifier la promo : " + mySelect.value + " ?")) {        
+        modifyPromotion(mySelect.value);        
     }
 })
 
